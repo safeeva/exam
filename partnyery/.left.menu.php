7012977b1e4d59d5f6995679e14fdbc1d2027561
Anastasia <udull@bk.ru>
2022-12-09 15:00:11 +0500

    ex 1, ex 2

---
+++ partnyery/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partnyery/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partnyery/kak-stat-partnerom.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий,", 
+		"/partnyery/raspisanie-meropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ partnyery/raspisanie-meropriyatiy/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Анонсы", 
+		"/partnyery/raspisanie-meropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Архив", 
+		"/partnyery/raspisanie-meropriyatiy/arkhiv.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>

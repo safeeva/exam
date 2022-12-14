ba5b5ba61501e509efd67826617f7996b5ded4ad
Anastasia <udull@bk.ru>
2022-12-14 10:27:05 +0500

    [ex1-11] Управление текстом в шаблоне блок Полезная информация - сам шаблон

---
+++ local/templates/exam1/components/bitrix/main.include/anonse_exam1/template.php
@@ -0,0 +1,24 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+$file = file_get_contents($arResult["FILE"]);
+if(($arResult["FILE"] <> '') && (strlen($file) > 0)){?>
+    <div class="side-block side-anonse">
+        <div class="title-block"><span class="i i-title01"></span>Полезная информация!</div>
+        <div class="item">
+            <?include($arResult["FILE"]);?>
+        </div>
+    </div>
+<?
+}?>

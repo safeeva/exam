ed9dc51f72677dbcb252dfa486bea4f947f1a494
Anastasia <udull@bk.ru>
2022-12-15 17:06:15 +0500

    [ex1-18] Форма обратной связи и почтовый шаблон: Новая форма с сайта. + настроила новый шаблон

---
+++ company/obratnaya-svyaz.php
@@ -0,0 +1,19 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Обратная связь");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback", 
+	".default", 
+	array(
+		"EMAIL_TO" => "test@mail.ru",
+		"EVENT_MESSAGE_ID" => array(
+			0 => "32",
+		),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array(
+		),
+		"USE_CAPTCHA" => "Y",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
